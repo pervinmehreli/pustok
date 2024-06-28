fb2c444ee7967849b46d9b49b8fa7a1eb1f64611
pervinmehreli <mehrliyevap@gmail.com>
2024-06-28 14:51:53 +0400

    first commit

---
+++ example-app/resources/views/firstproject.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Laravel Project</title>
+</head>
+<body >
+    <div style="background-color:red;" >
+        <h1 >Welcome</h1>
+
+    </div>
+    
+</body>
+</html>
